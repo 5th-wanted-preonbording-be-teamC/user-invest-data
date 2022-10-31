a5701d416c3aa6330e0c09b4e0850338c2bf42db
HoJin9622 <kiss0104040@gmail.com>
2022-10-31 17:46:29 +0900

    FEAT: 유저가 보유한 종목의 섹션 목록 API

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .models import Group
+from transactions.models import Transaction
+from .serializers import GroupSerializer
 
-# Create your views here.
+
+class AssetsGroupView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        """
+        유저가 보유한 종목의 섹션 목록을 불러옵니다.
+        GET api/v1/assets/group/
+        """
+
+        user = request.user
+        groups_id = (
+            Transaction.objects.filter(user=user)
+            .values_list("id", flat=True)
+            .distinct()
+        )
+        groups = Group.objects.filter(id__in=groups_id)
+        serializer = GroupSerializer(groups, many=True)
+        return Response(serializer.data)

c91119ef79cbd5790001f5e1d9b233181e425d46
HoJin9622 <kiss0104040@gmail.com>
2022-10-31 11:29:38 +0900

    FEAT: 유저 회원가입

--- users/views.py
+++ users/views.py
@@ -1,13 +1,7 @@
 from rest_framework.views import APIView
-
-
-class Me(APIView):
-    def get(self, request):
-        """
-        내 정보
-        GET /api/v1/users/me
-        """
-        pass
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+from .serializers import UserSerializer
 
 
 class Users(APIView):
@@ -15,6 +9,25 @@ class Users(APIView):
         """
         회원가입
         POST /api/v1/users
+        """
+
+        password = request.data.get("password")
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class Me(APIView):
+    def get(self, request):
+        """
+        내 정보
+        GET /api/v1/users/me
         """
         pass
 

93e38ddbd051c6bfb1bff80eca7e3d85424c8f6e
HoJin9622 <kiss0104040@gmail.com>
2022-10-31 10:54:15 +0900

    FEAT: view 및 url 설정

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class Me(APIView):
+    def get(self, request):
+        """
+        내 정보
+        GET /api/v1/users/me
+        """
+        pass
+
+
+class Users(APIView):
+    def post(self, request):
+        """
+        회원가입
+        POST /api/v1/users
+        """
+        pass
+
+
+class Login(APIView):
+    def post(self, request):
+        """
+        로그인
+        POST /api/v1/users/login
+        """
+        pass
+
+
+class Logout(APIView):
+    def post(self, request):
+        """
+        로그아웃
+        POST /api/v1/users/logout
+        """
+        pass

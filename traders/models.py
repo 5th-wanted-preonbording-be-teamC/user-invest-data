3ef0c1af9d2e71c72df8b5576234b07ad395b8f5
xxddongxx <hyeon2187@gmail.com>
2022-10-31 13:25:12 +0900

    FEAT: Trader 목록 api

--- traders/models.py
+++ traders/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class Traders(models.Model):
+class Trader(models.Model):
     company = models.CharField(max_length=30, unique=True, blank=False, verbose_name="증권사")
-    stock = models.ForeignKey(Stocks, on_delete=models.CASCADE, blank=True, verbose_name="주식종목")
-    account = models.ForeignKey(Accounts, on_delete=models.CASCADE, blank=True, verbose_name="계좌")
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE, blank=True, verbose_name="주식종목")
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, blank=True, verbose_name="계좌")
 
--- traders/views.py
+++ traders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from traders import serializers
+from traders.models import Trader
+
+
+class TradersView(APIView):
+    def get(self, request):
+        """
+        증권사 목록
+        GET /api/v1/traders/
+        """
+        traders = Trader.objects.all()
+        serializer = serializers.TradersSerializer(traders)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)

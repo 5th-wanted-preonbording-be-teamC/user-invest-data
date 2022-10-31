b4332617883d6dfb5e50cc21ba48491f5a63c494
HoJin9622 <kiss0104040@gmail.com>
2022-10-31 11:37:37 +0900

    FEAT: 로그인 & 로그아웃 API

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from .serializers import UserSerializer
 
@@ -21,7 +23,7 @@ class Users(APIView):
             user.save()
             serializer = UserSerializer(user)
             return Response(serializer.data)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return Response({"detail": serializer.errors}, status=HTTP_400_BAD_REQUEST)
 
 
 class Me(APIView):
@@ -33,6 +35,7 @@ class Me(APIView):
         내 정보
         GET /api/v1/users/me
         """
+
         user = request.user
         serializer = UserSerializer(user)
         return Response(serializer.data)
@@ -44,13 +47,29 @@ class Login(APIView):
         로그인
         POST /api/v1/users/login
         """
-        pass
+
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError("Username or password is required.")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return Response({"status": True})
+        return Response(
+            {"detail": "Invalid user data."},
+            status=HTTP_400_BAD_REQUEST,
+        )
 
 
 class Logout(APIView):
+
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         """
         로그아웃
         POST /api/v1/users/logout
         """
-        pass
+        logout(request)
+        return Response({"status": True})

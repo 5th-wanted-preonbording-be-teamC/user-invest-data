8349c6dd5d9f1a4825a9c1865ea6c8fa5a7b0829
xxddongxx <hyeon2187@gmail.com>
2022-10-31 12:50:22 +0900

    FEAT: Traders 모델 생성

--- traders/models.py
+++ traders/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Traders(models.Model):
+    company = models.CharField(max_length=30, unique=True, blank=False, verbose_name="증권사")
+    stock = models.ForeignKey(Stocks, on_delete=models.CASCADE, blank=True, verbose_name="주식종목")
+    account = models.ForeignKey(Accounts, on_delete=models.CASCADE, blank=True, verbose_name="계좌")
+

ce23330340941c1e8679001085ba10afd72a7823
HoJin9622 <kiss0104040@gmail.com>
2022-10-31 18:20:08 +0900

    FEAT: 유저가 보유한 섹션의 종목 API

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/users/", include("users.urls")),
     path("api/v1/assets/", include("assets.urls")),
+    path("api/v1/transactions/", include("transactions.urls")),
 ]

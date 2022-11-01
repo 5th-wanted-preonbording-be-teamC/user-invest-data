0f7ded0cff88cb6b914f4ccc8da12a16e6f88faf
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-01 10:44:15 +0900

    FEAT: Account 모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
+from traders.models import Trader
 
-# Create your models here.
+
+class Account(models.Model):
+    number = models.CharField(max_length=20, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trader = models.ForeignKey(Trader, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.account_name

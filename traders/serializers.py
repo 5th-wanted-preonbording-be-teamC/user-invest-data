d0f978bb6323b2ebe301855d8e41fbefefcb2142
xxddongxx <hyeon2187@gmail.com>
2022-11-01 13:56:59 +0900

    FEAT: 사용자의 증권회사 목록

--- traders/serializers.py
+++ traders/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from accounts.models import Account
 from traders.models import Trader
 
 
@@ -7,3 +8,9 @@ class DepositAndWithdrawal(serializers.ModelSerializer):
     class Meta:
         model = Trader
         fields = ["id", "is_deposit", "amount", "createdAt"]
+
+
+class TraderListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ["trader_id", "number"]

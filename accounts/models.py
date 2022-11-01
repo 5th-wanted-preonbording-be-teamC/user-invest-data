59e4faa7b371775e5ed0e6edfbd7409e47f4ab3a
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-01 11:03:58 +0900

    FEAT: verbose_name 추가

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,21 @@ from users.models import User
 
 
 class Account(models.Model):
-    number = models.CharField(max_length=20, primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # trader = models.ForeignKey(Trader, on_delete=models.CASCADE)
-    principal = models.PositiveBigIntegerField() # 투자원금
+    number = models.CharField(
+        max_length=20,
+        primary_key=True,
+        verbose_name="계좌번호",
+        )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="계좌주",
+    )
+    # trader = models.ForeignKey(
+    #     Trader,
+    #     on_delete=models.CASCADE,
+    # verbose_name="증권사")
+    principal = models.PositiveBigIntegerField(verbose_name="투자원금")
 
     def __str__(self):
         return f"{self.user}의 계좌 {self.number}"

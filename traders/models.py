dc009762b2410aacc990e4cec0d391762ad29f8c
xxddongxx <hyeon2187@gmail.com>
2022-11-01 20:53:29 +0900

    FIX: 증권사 모델 수정

--- traders/models.py
+++ traders/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-from users.models import User
-
 
 class Trader(models.Model):
-    company = models.CharField(
-        max_length=30, unique=True, blank=False, verbose_name="증권사"
+    name = models.CharField(max_length=30, unique=True, blank=False, verbose_name="증권사")
+    group_id = models.PositiveIntegerField(verbose_name="자산그룹_id")
+    amount = models.PositiveIntegerField(verbose_name="수량")
+    price = models.PositiveIntegerField(verbose_name="금액")
+    # 매수:True/매도:False
+    is_buy = models.BooleanField(blank=False, verbose_name="매수/매도")
+    transactions = models.ForeignKey(
+        "transactions.Transaction", on_delete=models.CASCADE, verbose_name="거래"
     )
-    is_deposit = models.BooleanField(blank=False, verbose_name="입/출금")
-    amount = models.PositiveIntegerField(verbose_name="입/출금 금액")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="사용자")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="입/출금 시간")

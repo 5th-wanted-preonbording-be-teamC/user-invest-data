7183d1593e24cc8ea5e60e82d1203c4865bc8da4
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-01 11:05:42 +0900

    DOCS: 타입 힌트 추가

--- accounts/models.py
+++ accounts/models.py
@@ -4,21 +4,23 @@ from users.models import User
 
 
 class Account(models.Model):
-    number = models.CharField(
+    number: models.CharField = models.CharField(
         max_length=20,
         primary_key=True,
         verbose_name="계좌번호",
         )
-    user = models.ForeignKey(
+    user: models.ForeignKey = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name="계좌주",
     )
-    # trader = models.ForeignKey(
+    # trader: models.ForeignKey = models.ForeignKey(
     #     Trader,
     #     on_delete=models.CASCADE,
     # verbose_name="증권사")
-    principal = models.PositiveBigIntegerField(verbose_name="투자원금")
+    principal: models.PositiveBigIntegerField = models.PositiveBigIntegerField(
+        verbose_name="투자원금",
+    )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.user}의 계좌 {self.number}"

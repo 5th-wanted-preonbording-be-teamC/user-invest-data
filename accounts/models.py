1d05777e524b4458bee1e9c82d225b23d1c9480a
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-01 10:49:07 +0900

    FIX: Account 모델 내 trader 필드 비활성화
    Trader 모델이 아직 완성되지 않았기 때문에 비활성화 했습니다.

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from users.models import User
-from traders.models import Trader
+# from traders.models import Trader
 
 
 class Account(models.Model):
     number = models.CharField(max_length=20, primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    trader = models.ForeignKey(Trader, on_delete=models.CASCADE)
+    # trader = models.ForeignKey(Trader, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.account_name

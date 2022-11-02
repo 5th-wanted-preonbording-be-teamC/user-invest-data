977cb2d3c5c8a19ef2007d062747f507598633cb
HoJin9622 <kiss0104040@gmail.com>
2022-11-02 10:36:20 +0900

    FIX: 총 자산 계산식 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,9 +10,5 @@ class AccountSerializer(serializers.ModelSerializer):
         fields = ("number", "name", "total_assets")
 
     def get_total_assets(self, obj):
-        total_assets = 0
-        total_assets += obj.principal
         transactions = obj.transactions.all()
-        for transaction in transactions:
-            total_assets += transaction.asset_price()
-        return total_assets
+        return sum(transaction.asset_price() for transaction in transactions)

4f9759d48aa9ba5456946f8f765ab55d7032e9d7
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-02 12:47:24 +0900

    FEAT: InvestDetailView 생성

--- invest/views.py
+++ invest/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
 from django.http import HttpResponseRedirect
 from accounts.models import Account, AccountOwner
-from accounts.serializers import AccountSerializer
+from accounts.serializers import AccountSerializer, AccountDetailSerializer
 
 
 class InvestsView(APIView):
@@ -13,6 +13,7 @@ class InvestsView(APIView):
     def get(self, request, pk):
         """
         GET /api/v1/invest/user/<int:pk>/
+
         투자 화면
         - 계좌명
         - 증권사
@@ -42,3 +43,41 @@ class RedirectToSelfView(APIView):
         """
 
         return HttpResponseRedirect(f"/api/v1/invest/users/{request.user.id}/")
+
+
+class InvestDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, user_pk, account_pk):
+        """
+        GET /api/v1/invest/user/<int:user_pk>/account/<int:account_pk>/
+
+        투자 상세 화면
+        - 계좌명
+        - 증권사
+        - 계좌번호
+        - 계좌 총 자산
+        - 투자 원금
+        - 총 수익금 (총 자산 - 투자 원금)
+        - 수익률 (총 수익금 / 투자 원금 * 100)
+        """
+
+        user = request.user
+        if user.id != user_pk:
+            return Response(HTTP_400_BAD_REQUEST)
+        filtered_owner = AccountOwner.objects.filter(user=user)
+        if not filtered_owner.exists():
+            # TODO: 계좌 등록 페이지로 연결
+            return Response({"message": "계좌 소유주로 등록되어 있지 않습니다."}, status=404)
+        owner = filtered_owner.first()
+        filtered_account = Account.objects.filter(
+            owner=owner,
+            number=account_pk,
+        )
+        if not filtered_account.exists():
+            return Response(
+                {"message": "{owner.name} 님의 {account_pk} 계좌를 조회할 수 없습니다."},
+                status=404,
+            )
+        serializer = AccountDetailSerializer(filtered_account, many=True)
+        return Response(serializer.data)

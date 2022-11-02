85c9f600cb1cd79389813b43d7c8b8c9ab4588d4
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-02 20:02:37 +0900

    FIX: AccountDetailSerializer 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,6 @@
+from django.db.models import QuerySet
 from rest_framework import serializers
+from transactions.models import Transaction
 from .models import Account
 
 
@@ -16,7 +18,6 @@ class AccountSerializer(serializers.ModelSerializer):
 
 class AccountDetailSerializer(serializers.ModelSerializer):
     total_assets = serializers.SerializerMethodField()
-    total_invested = serializers.SerializerMethodField()
     total_profit = serializers.SerializerMethodField()
     profit_rate = serializers.SerializerMethodField()
 
@@ -33,11 +34,11 @@ class AccountDetailSerializer(serializers.ModelSerializer):
         )
 
     def get_total_assets(self, obj: Account) -> int:
-        transactions = obj.transactions.all()
+        transactions: QuerySet[Transaction] = obj.transactions.all()
         return sum(transaction.asset_price() for transaction in transactions)
 
     def get_total_profit(self, obj: Account) -> int:
-        return self.get_total_assets(obj) - self.get_total_invested(obj)
+        return self.get_total_assets(obj) - obj.principal
 
-    def get_profit_rate(self, obj) -> float:
-        return self.get_total_profit(obj) / self.get_total_invested(obj) * 100
+    def get_profit_rate(self, obj: Account) -> float:
+        return self.get_total_profit(obj) / obj.principal * 100

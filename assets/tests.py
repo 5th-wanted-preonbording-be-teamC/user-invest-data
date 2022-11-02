d6dd43fb09449bffe5ce0e67eda785a479b8ce35
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-02 12:05:04 +0900

    FIX: 테스트 시 AccountOwner 생성

--- assets/tests.py
+++ assets/tests.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APITestCase
+from accounts.models import AccountOwner
 from users.models import User
 
 
@@ -9,6 +10,7 @@ class TestAssetsGroups(APITestCase):
         GET api/v1/assets/groups/
         """
 
+        # 비로그인 상태 요청
         response = self.client.get("/api/v1/assets/groups/")
         data = response.json()
 
@@ -17,9 +19,19 @@ class TestAssetsGroups(APITestCase):
             data["detail"], "Authentication credentials were not provided."
         )
 
+        # 유저 생성 및 로그인 상태 요청(계좌 소유자로 등록되어 있지 않은 경우)
         user = User.objects.create(username="test")
         self.client.force_login(user)
+        response = self.client.get("/api/v1/accounts/")
+        data = response.json()
+        self.assertEqual(response.status_code, 404)
 
+        # 계좌 소유주 생성 후 요청
+        AccountOwner.objects.get_or_create(
+            is_user=True,
+            name=user.name,
+            user=user,
+        )
         response = self.client.get("/api/v1/accounts/")
         data = response.json()
         self.assertEqual(response.status_code, 200)

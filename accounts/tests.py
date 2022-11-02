da43634a5c15962d7393cabe4317db2a6a8f4ac3
HoJin9622 <kiss0104040@gmail.com>
2022-11-02 10:48:59 +0900

    TEST: 보유 계좌 및 계좌 총 자산 테스트

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from users.models import User
 
-# Create your tests here.
+
+class TestAccounts(APITestCase):
+    def test_accounts(self):
+        """
+        유저가 보유한 계좌의 총 자산 테스트
+        GET api/v1/accounts/
+        """
+
+        response = self.client.get("/api/v1/accounts/")
+        data = response.json()
+
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            data["detail"], "Authentication credentials were not provided."
+        )
+
+        user = User.objects.create(username="test")
+        self.client.force_login(user)
+
+        response = self.client.get("/api/v1/accounts/")
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(data, list)

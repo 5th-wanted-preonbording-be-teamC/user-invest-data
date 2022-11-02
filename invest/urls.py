ce1e333f8bf0ca1c36d91544256c1a0d945d891c
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-02 19:51:22 +0900

    FIX: 오타 수정

--- invest/urls.py
+++ invest/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("users/", views.RedirectToSelfView.as_view()),
-    path("users/<int:user_pk>/", views.InvestsView.as_view()),
-    path("user/<int:user_pk>/account/<int:account_pk>/", views.InvestDetailView.as_view()),
+    path("user/", views.RedirectToSelfView.as_view()),
+    path("user/<int:user_pk>/", views.InvestsView.as_view()),
     path(
-        "user/<int:user_pk>/account/<int:account_pk>/accounts/",
+        "user/<int:user_pk>/account/<int:account_pk>/", views.InvestDetailView.as_view()
+    ),
+    path(
+        "user/<int:user_pk>/account/<int:account_pk>/assets/",
         views.InvestTransactionsView.as_view(),
     ),
 ]
